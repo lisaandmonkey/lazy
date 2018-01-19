9357238e42d09207512eaa6ee6b23ea665aae9ae
uninge <uninge@qq.com>
2018-01-19 18:10:13 +0800

    add: ok

---
+++ lazy.js
@@ -0,0 +1,166 @@
+/**
+ * Author: SunJingKe
+ * Email: uninge@qq.com
+ * Date: 2018.01.18
+ * Time: 16:24
+ */
+
+(function (global, factory) {
+    if (typeof exports === 'object' && typeof module !== 'undefined') {
+        module.exports = factory(global)
+    } else if (typeof define === 'function' && define.amd) {
+        define(factory)
+    } else {
+        global.Lazy = factory(global)
+    }
+})(this, function (global) {
+    // 全局变量，所有方法在此对象上扩展
+    var Lazy = {};
+    // 计时器
+    var timer = null;
+    // 节流延迟时间
+    var delay = 150;
+    // 是否开启节流
+    var debounce = true;
+    // 是否开启图片懒加载图片的重载。解释一下：就是图片离开懒加载区域要把图片状态复原，再次进入懒加载区域要在视觉上呈现懒加载效果
+    // 先呵呵一下这个功能，正常的我肯定想不到这么个抽风的需求，谁让我曾经碰到过一个抽风的产品经理呢，实现不难，这里也顺便实现一下
+    var unload = false;
+    // 回掉函数
+    var callback = function () {};
+    // 元素相对于视窗的位置集合
+    var boxRect = {};
+    /**
+     * 判断目标元素是否可见
+     * https://developer.mozilla.org/zh-CN/docs/Web/API/HTMLElement/offsetParent
+     * @param {HTMLElement} element
+     * @returns {boolean}
+     */
+    var isHidden = function (element) {
+        return element.offsetParent === null;
+    };
+    /**
+     * 判断目标元素是否进入懒加载区域
+     * @param {HTMLElement} element
+     * @returns {boolean}
+     */
+    var canLoadImg = function (element) {
+        if (isHidden(element)) return false;
+        var eleRect = element.getBoundingClientRect();
+        return (eleRect.top <= boxRect.b && eleRect.right >= boxRect.l && eleRect.bottom >= boxRect.t && eleRect.left <= boxRect.r);
+    };
+    /**
+     * 节流函数
+     */
+    var onDebounceRender = function () {
+        if (!debounce) {
+            Lazy.render();
+        } else {
+            clearTimeout(timer);
+            timer = setTimeout(function () {
+                Lazy.render();
+                timer = null;
+            }, delay);
+        }
+    };
+
+    /**
+     * 始化方法，外部直接调用，配置参数在此接收
+     * @param {Object} options
+     * @param {String} options.root - 图片滚动区域根元素选择器
+     * @param {Number} options.offset - 懒加载阈值，当没有上下左右有4个值时将以此为准
+     * @param {Number} options.offsetTop - 懒加载阈值【上】
+     * @param {Number} options.offsetRight - 懒加载阈值【右】
+     * @param {Number} options.offsetBottom - 懒加载阈值【下】
+     * @param {Number} options.offsetLeft - 懒加载阈值【左】
+     * @param {Boolean} options.debounce - 是否开启函数节流
+     * @param {Number} options.delay - 函数节流时间阈值
+     * @param {Boolean} options.unload - 图片重载
+     * @param {Function} options.callback - 懒加载操作完成时的回掉函数
+     */
+    Lazy.init = function (options) {
+        options = options || {};
+        global = document.querySelector(options.root) || global;
+        debounce = options.debounce !== false;
+        delay = options.delay || delay;
+        unload = options.unload || unload;
+        callback = options.callback || callback;
+        // 懒加载区域，写的虽然有点长但是不难理解
+        boxRect = {
+            t: 0 - (options.offsetTop || options.offset || 0),
+            r: (global.innerWidth || document.documentElement.clientWidth) + (options.offsetRight || options.offset || 0),
+            b: (global.innerHeight || document.documentElement.clientHeight) + (options.offsetBottom || options.offset || 0),
+            l: 0 - (options.offsetLeft || options.offset || 0)
+        };
+        // 这里提前调用一次，因为如果debounce为true，load之后会有最低250ms的延迟
+        Lazy.render();
+        // 绑定事件
+        if (global.addEventListener) {
+            global.addEventListener('load', onDebounceRender, false);
+            global.addEventListener('scroll', onDebounceRender, false);
+        } else {
+            global.attachEvent('onload', onDebounceRender);
+            global.attachEvent('onscroll', onDebounceRender);
+        }
+    };
+    /**
+     * 懒加载实现
+     * @param {HTMLElement} element
+     */
+    Lazy.render = function (element) {
+        var nodes = (element || document).querySelectorAll('[data-lazy], [data-lazy-background]');
+        var len = nodes.length;
+        for (var i = 0; i < len; i++) {
+            var node = nodes[i];
+            // 目标元素在懒加载区域和不在懒加载区域
+            if (canLoadImg(node)) {
+                // 懒加载图片需要重载
+                if (unload && node.src && !node.getAttribute('data-lazy-placeholder')) {
+                    node.setAttribute('data-lazy-placeholder', node.src);
+                }
+                // 图片的懒加载
+                var src = node.getAttribute('data-lazy');
+                if (src !== null && node.src !== src) {
+                    node.src = src;
+                }
+                // 背景的懒加载
+                var bgUrl = node.getAttribute('data-lazy-background');
+                if (bgUrl !== null && node.style.backgroundImage !== bgUrl) {
+                    node.style.backgroundImage = 'url(' + bgUrl + ')';
+                }
+                // 如果图片不需要重载，懒加载完成移除data-lazy属性
+                if (!unload) {
+                    node.removeAttribute('data-lazy');
+                }
+                // 懒加载完成移除data-lazy-background
+                node.removeAttribute('data-lazy-background');
+                // 懒加载完成触发回掉
+                callback(node, 'load');
+            } else {
+                // 当图片不在懒加载区域时做重载处理
+                var placeholder = node.getAttribute('data-lazy-placeholder');
+                if (unload && placeholder !== null) {
+                    node.src = placeholder;
+                    node.removeAttribute('data-lazy-placeholder');
+                    // 重载完成触发回掉
+                    callback(node, 'unload');
+                }
+            }
+        }
+        // 如果没有懒加载的元素，销毁Lazy.init添加的事件
+        if (len === 0) {
+            Lazy.destroy();
+        }
+    };
+    /**
+     * 不满足懒加载条件时移除绑定的事件，重置定时器引用
+     */
+    Lazy.destroy = function () {
+        if (document.removeEventListener) {
+            global.removeEventListener('scroll', onDebounceRender);
+        } else {
+            global.detachEvent('onscroll', onDebounceRender);
+        }
+        clearTimeout(timer);
+    };
+    return Lazy;
+});
---
+++ readme.md
@@ -0,0 +1,113 @@
+## Lazy.js
+
+这是一个非常小巧的图片懒加载库，未压缩源码只有100行左右，写来用在了自己的一个小项目中，注释很详细，君可随意更改。
+
+> Lazy.js的使用非常简单，只需要按照如下格式即可：
+
+```html
+<!--<div class="s-pic-wrapper pic-wrapper">-->
+    <img class="img" src="./assets/loading.png" data-lazy="./assets/01.jpg">
+    <div class="bg-img" data-lazy-background="./assets/02.jpg"></div>
+    <img class="img" src="./assets/loading.png" data-lazy="./assets/03.jpg">
+    <div class="bg-img" data-lazy-background="./assets/04.jpg"></div>
+    <img class="img" src="./assets/loading.png" data-lazy="./assets/05.jpg">
+    <div class="bg-img" data-lazy-background="./assets/06.jpg"></div>
+    <img class="img" src="./assets/loading.png" data-lazy="./assets/07.jpg">
+    <div class="bg-img" data-lazy-background="./assets/08.jpg"></div>
+    <img class="img" src="./assets/loading.png" data-lazy="./assets/09.jpg">
+    <div class="bg-img" data-lazy-background="./assets/10.jpg"></div>
+    <img class="img" src="./assets/loading.png" data-lazy="./assets/11.jpg">
+    <div class="bg-img" data-lazy-background="./assets/12.jpg"></div>
+    <img class="img" src="./assets/loading.png" data-lazy="./assets/13.jpg">
+    <div class="bg-img" data-lazy-background="./assets/14.jpg"></div>
+    <img class="img" src="./assets/loading.png" data-lazy="./assets/15.jpg">
+    <div class="bg-img" data-lazy-background="./assets/16.jpg"></div>
+    <img class="img" src="./assets/loading.png" data-lazy="./assets/17.jpg">
+    <div class="bg-img" data-lazy-background="./assets/18.jpg"></div>
+    <img class="img" src="./assets/loading.png" data-lazy="./assets/19.jpg">
+    <div class="bg-img" data-lazy-background="./assets/20.jpg"></div>
+<!--</div>-->
+
+<script src="lazy.js"></script>
+<script>
+    Lazy.init({
+        // root: '.s-pic-wrapper', // 滚动区域根元素，默认document
+        debounce: true,            // 函数节流
+        delay: 200,                // 节流延时
+        unload: false,             // 图片重载
+        offset: 200,               // 懒加载距视口距离
+        offsetTop: 200,            // 可选，默认offset
+        offsetRight: 200,          // 可选，默认offset
+        offsetBottom: 200,         // 可选，默认offset
+        offsetLeft: 200,           // 可选，默认offset
+        // 完成时的回掉函数
+        callback: function (element, status) {
+            console.log(element, status);
+        }
+    });
+</script>
+```
+
+#### 参数说明
+
+> Lazy.init(options)
+
+所有配置参数在懒加载初始化`.init()`函数中接收。
+
+* root: Type `HTMLElement`, Default: `document`
+
+滚动区域根元素
+
+* debounce: Type: `Boolean`, Default: `true`
+
+是否开启函数节流
+
+* delay: Type `Number`, Default: `150`
+
+节流函数执行延时时间
+
+* unload: Type: `Boolean`, Default: `false`
+
+懒加载图片是否重载
+
+* offset: Type: `Number`, Default: `0`
+
+此参数用来控制懒加载元素距离视口的距离。
+
+* offsetTop: Type: `Number`, Default: `offset`
+   
+此参数用来控制懒加载元素距离视口顶部的距离。
+
+* offsetRight: Type: `Number`, Default: `offset`
+   
+此参数用来控制懒加载元素距离视口右侧的距离。
+
+* offsetBottom: Type: `Number`, Default: `offset`
+   
+此参数用来控制懒加载元素距离视口底部的距离。
+
+* offsetLeft: Type: `Number`, Default: `offset`
+   
+此参数用来控制懒加载元素距离视口左侧的距离。
+
+* callback: Type: `Function`, Default: `function(){}`
+
+> Lazy.render()
+
+内部函数，外部不可调用。
+
+> Lazy.destroy()
+
+内部函数，外部不可调用。
+
+#### html配置
+
+请确保html部分按如下格式配置：
+
+```html
+<img src="./assets/loading.png" data-lazy="./assets/01.jpg">
+<div data-lazy-background=".assets/02.jpg"></div>
+```
+
+> 本项目还有一篇对应的[博文](https://github.com/uninge/blog-post/tree/master/lazy)
+
